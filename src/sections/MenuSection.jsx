5a4ae432e28ad198bc71c78554c1b6e108dd7fe6
Basem Mahmoud <basem556473@gmail.com>
2026-07-06 10:24:18 +0300

    w

--- src/sections/MenuSection.jsx
+++ src/sections/MenuSection.jsx
@@ -1,4 +1,4 @@
-import MarqueeSkill from "../components/MarqueeSkill";
+import MarqueeSkill from "../Components/MarqueeSkill";
 import image1 from "../assets/img1.jpg";
 import image2 from "../assets/img2.jpg";
 import image3 from "../assets/img3.jpg";
